d277003bdb4a151a84c934eeed6d7ee37120964a
Jacob <jbmenashi@gmail.com>
2019-10-01 11:32:15 -0400

    associative arrays

---
+++ assoc_arrays.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Associative Arrays</title>
+</head>
+<body>
+   <?php
+      $grades = array("Bob" => 100, "Jim" => 95, "Steve" => 90);
+      echo $grades["Bob"]; // so it's an object
+   ?>
+</body>
+</html>
