f16efd17538908e013aed8b7b03b5039a1ebcf08
Jacob <jbmenashi@gmail.com>
2019-10-01 10:25:12 -0400

    first commit

---
+++ start.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title></title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="">
+   </head>
+   <body>
+      <?php
+         echo "<h1>This is PHP</h1>";
+         echo "<hr>";
+         echo "<h3>The Language of the Past</h3>";
+      ?>
+   </body>
+</html>
---
+++ variables.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title></title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="">
+   </head>
+   <body>
+      <?php
+      $name = "Jacob";
+      echo "<h3>My name is $name</h3>";
+      $name = "Not Jacob";
+      echo "<h4>Now it is $name</h4>";
+      ?>
+   </body>
+</html>
